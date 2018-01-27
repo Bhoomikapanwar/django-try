cc06773a5a29c7d7ab56754435e9809ae4393d13
shivam2146 <shivam.mcs17.du@gmail.com>
2018-01-27 17:11:44 +0530

    use template and template tag ,filter

--- src/try/views.py
+++ src/try/views.py
@@ -1,7 +1,14 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 #function based views
 def home(request):
-    return HttpResponse('hello welcome to home')
+    num = random.randint(0,1000)
+    check = True               #is used in html to conditional rendering of html tags
+    myvar = "it works"
+    list1= [random.randint(0,1000000),random.randint(0,100000),random.randint(0,100000)]    #will be used for iterating in html
+    context = {'myvar':myvar,'num':num, 'check':check,'list1':list1}                 #context dict passed as context to render
+    return render(request,"base.html",context)       #rendering htmls from template folder
+    #return HttpResponse('hello welcome to home')   #simple HttpResponse
