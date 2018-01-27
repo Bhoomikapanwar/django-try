251bbab194fcc42ab84dca171c684512c577bd9a
shivam2146 <shivam.mcs17.du@gmail.com>
2018-01-27 14:32:51 +0530

    remove folder from repo

--- src/myproject/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import *         #imports all from base.py its imp as now base.py will be used as setting.py
-
-from .production import *
-
-try:
-    from .local import *
-except:
-    pass
