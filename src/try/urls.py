a7f0fbb502592730515788f5fbf9d2be34c16ade
shivam2146 <shivam.mcs17.du@gmail.com>
2018-01-27 20:02:40 +0530

    use template class view

--- src/try/urls.py
+++ src/try/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path,include
-from .views import home, contact, about, ContactView
+from .views import home, contact, about, ContactView,AboutTemplateView,HomeTemplateView
+from django.views.generic import TemplateView
 
 urlpatterns= [
         #using function based views
-        path('home/',home,name='home'),
-        path('',home,name='home'),
+        #path('home/',home,name='home'),
+        #path('',home,name='home'),
         #path('contact/',contact,name='contact'),
-        path('about/',about,name='about'),
+        #path('about/',about,name='about'),
 
         #using class based views
         path('contact/',ContactView.as_view(),name='contact'),
+
+        #using class based template view
+        path('about/',AboutTemplateView.as_view(),name='about'),
+        path('home/',HomeTemplateView.as_view(),name='home1'),
+
+        #another efficient way of using class based template view but here context is not there
+        path('',TemplateView.as_view(template_name="home.html"))
 ]
--- src/try/views.py
+++ src/try/views.py
@@ -2,6 +2,7 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View       #for class based view
+from django.views.generic import TemplateView
 
 
 #function based views
@@ -23,6 +24,22 @@ def about(request):
     context = {}
     return render(request,"about.html",context)
 
+#class based view
 class ContactView(View):
     def get(self,request,*args,**kwargs):
         return render(request,"contact.html",{})
+#class based template view
+class AboutTemplateView(TemplateView):
+    template_name = 'about.html'
+
+#class based template view overriding get_context_data method 
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self,*args,**kwargs):
+        context= super(HomeTemplateView,self).get_context_data(*args,**kwargs)
+        num = random.randint(0,1000)
+        check = True               #is used in html to conditional rendering of html tags
+        myvar = "it works"
+        list1= [random.randint(0,1000000),random.randint(0,100000),random.randint(0,100000)]    #will be used for iterating in html
+        context = {'myvar':myvar,'num':num, 'check':check,'list1':list1}
+        return context
