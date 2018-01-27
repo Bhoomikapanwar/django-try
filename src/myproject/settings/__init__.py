143fdc49739fabb05783847c82b297ed5d3c2e08
shivam2146 <shivam.mcs17.du@gmail.com>
2018-01-27 17:15:18 +0530

    comment production and local import

--- src/myproject/settings/__init__.py
+++ src/myproject/settings/__init__.py
@@ -1,8 +1,8 @@
 from .base import *         #imports all from base.py its imp as now base.py will be used as setting.py
 
-from .production import *
+#from .production import *
 
-try:
-    from .local import *
-except:
-    pass
+#try:
+#    from .local import *
+#except:
+#    pass
