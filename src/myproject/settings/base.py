7ca51a512b8fda4ae2ff9917dcfbf509f25adf1d
shivam2146 <shivam.mcs17.du@gmail.com>
2018-01-27 17:16:54 +0530

    change DIRS for TEMPLATES to locate templates folder containing html files

--- src/myproject/settings/base.py
+++ src/myproject/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))     
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))    #it is where manage.py is
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],           #it adds tempates folder to be used for searching templates from
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
