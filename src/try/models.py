cd90011769e18570c7e6225586a0290df88bfbcc
shivam2146 <shivam.mcs17.du@gmail.com>
2018-02-01 01:35:04 +0530

    add fields to project model

--- src/try/models.py
+++ src/try/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=120)
-    use = models.CharField(max_length=120, null = True, blank = True)
+    name        = models.CharField(max_length=120)
+    use         = models.CharField(max_length=120, null = True, blank = True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateField(auto_now= True)
